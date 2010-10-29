31fb87ed8850b2106a3ff4ee698b3c47e04d3716
Magnus Andersson <magnus@magnusart.com>
2010-10-30 00:51:02 +0200

    Prior to moving ConsumerDetails, PersonDetails and Company details to CommonModel
    Signed-off-by: Magnus Andersson <magnus@magnusart.com>

--- ConsumerMasterPortal/src/main/java/com/magnusart/crm/portal/server/ConsumerServiceImpl.java
+++ ConsumerMasterPortal/src/main/java/com/magnusart/crm/portal/server/ConsumerServiceImpl.java
@@ -1,18 +1,22 @@
 package com.magnusart.crm.portal.server;
 
 import javax.ejb.EJB;
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.magnusart.crm.business.ConsumerBusinessServices;
+import com.magnusart.crm.business.commands.CreateConsumerCommand;
+import com.magnusart.crm.model.e.ConsumerEntity;
 import com.magnusart.crm.portal.client.ConsumerService;
+import com.magnusart.crm.portal.server.converter.ConsumerConverter;
+import com.magnusart.crm.portal.shared.CompanyDetails;
 import com.magnusart.crm.portal.shared.ConsumerDetails;
+import com.magnusart.crm.portal.shared.PersonDetails;
 
 /**
- * The Consumer Servie for GWT
+ * The Consumer Service entry. This is where any server side logic (filtering,
+ * conversion, error handling etc) that is specific for the Portal is put.
  * 
  * @author Magnus Andersson
  * @since 17 okt 2010
@@ -28,23 +32,27 @@ public class ConsumerServiceImpl extends RemoteServiceServlet implements
 	@Override
 	public void init(final ServletConfig config) throws ServletException {
 		super.init(config);
-
-		try {
-			// get the inital context
-			final Context ctx = new InitialContext();
-
-			services = (ConsumerBusinessServices) ctx
-					.lookup("ConsumerBusinessServices");
-		} catch (final Exception e) {
-			e.printStackTrace();
-		}
 	}
 
 	@Override
 	public ConsumerDetails createConsumer(final ConsumerDetails consumer) {
-		System.out.println(consumer.toString());
-		final String answer = services.testService(consumer.toString());
-		System.out.println(answer);
+
+		final CreateConsumerCommand command = new CreateConsumerCommand();
+
+		ConsumerEntity consumerEntity = null;
+
+		if (consumer instanceof PersonDetails) {
+			consumerEntity = ConsumerConverter
+					.convert((PersonDetails) consumer);
+		} else if (consumer instanceof CompanyDetails) {
+			consumerEntity = ConsumerConverter
+					.convert((CompanyDetails) consumer);
+		} else {
+			assert false : "Should always be either Company or Person details";
+		}
+
+		services.createConsumer(consumerEntity);
+
 		return consumer;
 	}
 
