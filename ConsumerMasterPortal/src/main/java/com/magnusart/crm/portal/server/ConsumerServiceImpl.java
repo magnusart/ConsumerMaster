40f60a18210f403aef5316bee7afe10c10f27b9c
Magnus Andersson <magnus@magnusart.com>
2010-10-30 16:27:49 +0200

    Added some handlers

--- ConsumerMasterPortal/src/main/java/com/magnusart/crm/portal/server/ConsumerServiceImpl.java
+++ ConsumerMasterPortal/src/main/java/com/magnusart/crm/portal/server/ConsumerServiceImpl.java
@@ -4,12 +4,14 @@ import javax.ejb.EJB;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 
+import com.google.gwt.event.shared.HandlerManager;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
-import com.magnusart.crm.business.ConsumerBusinessServices;
-import com.magnusart.crm.business.commands.CreateConsumerCommand;
-import com.magnusart.crm.model.e.ConsumerEntity;
+import com.magnusart.crm.CommandBus;
+import com.magnusart.crm.business.commands.CreatePersonCommand;
+import com.magnusart.crm.business.presentation.AbstractPresentation;
+import com.magnusart.crm.business.presentation.PresentPerson;
+import com.magnusart.crm.business.presentation.PresentationStateHandler;
 import com.magnusart.crm.portal.client.ConsumerService;
-import com.magnusart.crm.portal.server.converter.ConsumerConverter;
 import com.magnusart.crm.portal.shared.CompanyDetails;
 import com.magnusart.crm.portal.shared.ConsumerDetails;
 import com.magnusart.crm.portal.shared.PersonDetails;
@@ -22,38 +24,53 @@ import com.magnusart.crm.portal.shared.PersonDetails;
  * @since 17 okt 2010
  */
 public class ConsumerServiceImpl extends RemoteServiceServlet implements
-		ConsumerService {
+		ConsumerService, PresentationStateHandler {
 
 	private static final long serialVersionUID = 1L;
 
 	@EJB
-	ConsumerBusinessServices services;
+	private CommandBus services;
 
+	private PresentPerson personPage = null;
+
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void init(final ServletConfig config) throws ServletException {
 		super.init(config);
+		final HandlerManager eventBus = services.getEventBus();
+		eventBus.addHandler(AbstractPresentation.TYPE, this);
+
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public ConsumerDetails createConsumer(final ConsumerDetails consumer) {
 
-		final CreateConsumerCommand command = new CreateConsumerCommand();
-
-		ConsumerEntity consumerEntity = null;
-
 		if (consumer instanceof PersonDetails) {
-			consumerEntity = ConsumerConverter
-					.convert((PersonDetails) consumer);
+			final CreatePersonCommand command = new CreatePersonCommand(
+					(PersonDetails) consumer);
+			services.executeCommand(command);
+
 		} else if (consumer instanceof CompanyDetails) {
-			consumerEntity = ConsumerConverter
-					.convert((CompanyDetails) consumer);
+			// Do nothing yet
 		} else {
 			assert false : "Should always be either Company or Person details";
 		}
 
-		services.createConsumer(consumerEntity);
+		if (personPage != null) {
+			System.out.println("We got an Person Page!");
+		}
 
 		return consumer;
+	}
+
+	@Override
+	public void onPersonPage(final PresentPerson event) {
+		personPage = event;
 	}
 
 }
